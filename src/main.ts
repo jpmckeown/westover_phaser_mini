1cc58212a2bb8cc2d89e2480c7f624590a3bfc5a
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2025-05-08 07:06:59 +0100

    add vertical gridlines; adjust text

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,28 @@ class Game extends Phaser.Scene {
 
   create(): void {
     this.add.image(50, 50, SPRITE_ASSET_KEY, 2);
+    this.add.text(240, 50, 'Tic-Tac', {
+      fontSize: '42px',
+      fontFamily: 'Verdana',
+      color: 'purple',
+      align: 'center',
+    }).setOrigin(0.5);
+    this.add.text(240, 600, 'X turn', {
+      fontSize: '22px',
+      fontFamily: 'Verdana',
+      color: 'black',
+      align: 'center',
+    }).setOrigin(0.5);
+
+    const graphics = this.add.graphics();
+    let left = 170;
+    let top = 120;
+    let gap = 140
+    graphics.lineStyle(12, 0x3e3e3e);
+    for (let i = 0; i < 3; i++) {
+      graphics.lineBetween(left + i*gap, top, left + i*gap, top + 2*gap);      
+    }
+
   }
 }
 
